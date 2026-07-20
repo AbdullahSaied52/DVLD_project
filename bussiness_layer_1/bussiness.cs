60f21e46e76cdc841a5c6dd24dcdb97a521578d0
AbdullahSaied52 <a9407602@gmail.com>
2026-07-20 10:10:09 +0300

    create a bussiness layer

---
+++ bussiness_layer_1/bussiness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClsPerson_namspace;
+using data_layer_1;
+
+namespace bussiness_layer_1
+{
+    public class ClsBussiness
+    {
+        public static List<ClsPerson> list_all()
+        {
+            return ClsData.list_all();
+        }
+    }
+}
